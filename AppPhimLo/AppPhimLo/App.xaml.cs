d3c3f9ccee887254ddfd26994ab459bce45b5677
Le Tuan Phat <21129858@st.hcmuaf.edu.vn>
2025-10-02 11:56:22 +0700

    khoi tao du an

---
+++ AppPhimLo/AppPhimLo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppPhimLo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
