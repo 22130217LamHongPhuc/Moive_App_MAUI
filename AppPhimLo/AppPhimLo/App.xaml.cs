0ab76e75962fba40b2d9af7259121d7404759fb3
Võ Phượng Tường <22130316@st.hcmuaf.edu.vn>
2025-10-04 12:05:49 +0700

    1

---
+++ AppPhimLo/AppPhimLo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AppPhimLo;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ AppPhimLo/AppPhimLo/ViewModels/SearchViewModel .cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using AppPhimLo.Models;
+using AppPhimLo.Services;
+
+namespace AppPhimLo.ViewModels;
+
+public class SearchViewModel : BindableObject
+{
+    private string searchQuery;
+    private int currentPage = 1;
+    private int totalPages = 1;
+    private bool isLoading = false;
+    private const int PageSize = 10;
+
+    private readonly MovieService _movieService;
+
+    public string SearchQuery
+    {
+        get => searchQuery;
+        set { searchQuery = value; OnPropertyChanged(); }
+    }
+
+    public int CurrentPage
+    {
+        get => currentPage;
+        set { currentPage = value; OnPropertyChanged(); UpdatePaginationProperties(); }
+    }
+
+    public int TotalPages
+    {
+        get => totalPages;
+        set { totalPages = value; OnPropertyChanged(); UpdatePaginationProperties(); }
+    }
+
+    public ObservableCollection<Movie> Movies { get; set; } = new();
+
+    public bool HasMovies => Movies.Count > 0;
+    public bool CanGoPrevious => CurrentPage > 1;
+    public bool CanGoNext => CurrentPage < TotalPages;
+
+    public ICommand SearchCommand { get; }
+    public ICommand NextPageCommand { get; }
+    public ICommand PreviousPageCommand { get; }
+
+    public SearchViewModel()
+    {
+        _movieService = new MovieService();
+
+        SearchCommand = new Command(async () => await SearchMoviesAsync(1));
+
+        NextPageCommand = new Command(async () =>
+        {
+            if (CanGoNext)
+                await SearchMoviesAsync(CurrentPage + 1);
+        });
+
+        PreviousPageCommand = new Command(async () =>
+        {
+            if (CanGoPrevious)
+                await SearchMoviesAsync(CurrentPage - 1);
+        });
+    }
+
+    private async Task SearchMoviesAsync(int page)
+    {
+        if (string.IsNullOrWhiteSpace(SearchQuery) || isLoading)
+            return;
+
+        isLoading = true;
+        CurrentPage = page;
+
+        Movies.Clear();
+
+        var (results, totalItems, totalPages) = await _movieService.SearchMoviesAsync(SearchQuery, CurrentPage, PageSize);
+
+        foreach (var movie in results)
+            Movies.Add(movie);
+
+        TotalPages = totalPages; 
+
+        OnPropertyChanged(nameof(Movies));
+        OnPropertyChanged(nameof(HasMovies));
+        UpdatePaginationProperties();
+
+        isLoading = false;
+    }
+
+    private void UpdatePaginationProperties()
+    {
+        OnPropertyChanged(nameof(CanGoPrevious));
+        OnPropertyChanged(nameof(CanGoNext));
+    }
+}
